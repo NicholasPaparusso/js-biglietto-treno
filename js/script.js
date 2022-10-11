04a16f1fe6e5d9dcbd4bd522e1ecd7f522be63d7
NicholasPaparusso <nicopaparusso@gmail.com>
2022-10-11 17:12:11 +0200

    [add] error message

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
 //Assegnazione Variabili
 const nKm = parseInt(prompt('Inserire chilometri da percorrere?'));
 const userAge = parseInt(prompt('Inserire età'));
+let validInput = true;
+
+if(userAge > 110 || userAge < 1 || nKm > 3000 || nKm < 1){
+  alert ('Inserisci valori congrui');
+  validInput = false;
+}
+
+
 const discount_1 = 0.2 ;
 const discount_2 = 0.4 ;
 const priceKm = 0.21;
 let priceTicket = nKm * priceKm;
-let fixedPrice = priceTicket.toFixed(2);
-if(userAge <= 18){
-  fixedPrice = priceTicket - priceTicket * discount_1;
-}else if (userAge >= 65){
-  fixedPrice = priceTicket -priceTicket * discount_2;
-}
 
-console.log("prezzo biglietto con n decimali " + priceTicket )
-console.log("prezzo biglietto con 2 decimali " + fixedPrice )
-console.log( "chilometri da percorrere " + nKm );
-console.log("età utente " + userAge);
+
+if(validInput){
+if(userAge <= 18){
+  priceTicket = priceTicket - priceTicket * discount_1;
+}else if (userAge >= 65){
+  priceTicket = priceTicket - priceTicket * discount_2;
+}
+}
+let fixedPrice = priceTicket.toFixed(2);
+
+console.log('input non valido' + validInput);
+document.getElementById('ticket').innerHTML =  `Il costo del biglietto sarà ${fixedPrice} €`;
+
+if(validInput = false){
+  document.getElementById('error').innerHTML = `Ricaricare la pagina ed inserire nuovamente i dati, la informiamo che la distanza massima percorribile da questa destinazione è di 3000km`;
+}
